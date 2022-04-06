1dfd4b99f28be9b02351e3fe7d929bd30ea6ad6d
sanket-walanj <code.sanky@gmail.com>
2022-04-07 02:30:36 +0530

    last

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
-const count = document.querySelector('.count');
-const input = document.querySelector('input');
+const tabLinks = document.getElementsByClassName('tab-link');
+const allContent = document.querySelectorAll('.tab-content');
 
-input.addEventListener('keyup', () => {
-  count.innerHTML = input.value.length;
-});
+for (let i = 0; i < tabLinks.length; i++) {
+  tabLinks[i].addEventListener('click', function (e) {
+    const current = document.getElementsByClassName('active');
+    current[0].className = current[0].className.replace('active', '');
+    this.className += ' active';
+
+    // Switch Content
+    const filter = e.target.dataset.filter;
+    console.log(filter);
+
+    allContent.forEach((content) => {
+      if (content.classList.contains(filter)) {
+        content.style.display = 'block';
+      } else {
+        content.style.display = 'none';
+      }
+    });
+  });
+}
